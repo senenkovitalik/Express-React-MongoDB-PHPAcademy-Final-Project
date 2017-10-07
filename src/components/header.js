ba3d8e570107e26d3c8c962a945d1d04ad3e3d66
senenkovitalik <senenkovitalik@gmail.com>
2017-10-07 22:07:53 +0300

    Header and Sign up modal - reactstrap way

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
-  Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink,
-  Form, Label, Input, Button
+  Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter
 } from 'reactstrap';
 
 class Header extends React.Component {
@@ -9,9 +8,12 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      modal: false
     };
+
     this.toggle = this.toggle.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   toggle() {
@@ -20,68 +22,64 @@ class Header extends React.Component {
     });
   }
 
+  toggleModal() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   render() {
     return (
+      <Navbar color="dark" dark expand="xl">
+        <NavbarBrand href="/">GuitarShop</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/" active>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact_us">Contact us</NavLink>
+            </NavItem>
+          </Nav>
+          <Form inline className="my-2 my-lg-0 flex-wrap">
+            <Label for="login" style={{color: 'white', marginRight: 10+'px'}}>Login</Label>
+            <Input id="login" className="mr-sm-2" type="text" placeholder="Login, email or phone number" />
+            <Label for="passwr" style={{color: 'white', marginRight: 10+'px'}}>Password</Label>
+            <Input id="passwr" className="mr-sm-2" type="password" placeholder="Password" />
+            <Button color="success" className="my-2 my-sm-0" style={{marginRight: 10+'px'}}>Log in</Button>
+            <Button onClick={this.toggleModal} color="primary" className="my-2 my-sm-0">Sign up</Button>
+          </Form>
+        </Collapse>
 
-    <Navbar color="dark" dark expand="xl">
-      <NavbarBrand href="/">GuitarShop</NavbarBrand>
-      <NavbarToggler onClick={this.toggle} />
-      <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="/" active>Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/contact_us">Contact us</NavLink>
-          </NavItem>
-        </Nav>
-        <Form inline className="my-2 my-lg-0 flex-wrap">
-          <Label for="login" style={{color: 'white', marginRight: 10+'px'}}>Login</Label>
-          <Input id="login" className="mr-sm-2" type="text" placeholder="Login, email or phone number" />
-          <Label for="passwr" style={{color: 'white', marginRight: 10+'px'}}>Password</Label>
-          <Input id="passwr" className="mr-sm-2" type="password" placeholder="Password" />
-          <Button color="success" className="my-2 my-sm-0" style={{marginRight: 10+'px'}}>Log in</Button>
-          <Button color="primary" className="my-2 my-sm-0">Sign up</Button>
-        </Form>
-      </Collapse>
-      {/*<!-- Sign up Modal -->*/}
-      {/*<div className="modal fade" id="signupModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">*/}
-      {/*<div className="modal-dialog" role="document">*/}
-      {/*<div className="modal-content">*/}
-      {/*<div className="modal-header">*/}
-      {/*<h5 className="modal-title" id="exampleModalLabel">Sign up</h5>*/}
-      {/*<button type="button" className="close" data-dismiss="modal" aria-label="Close">*/}
-      {/*<span aria-hidden="true">&times;</span>*/}
-      {/*</button>*/}
-      {/*</div>*/}
-      {/*<div className="modal-body">*/}
-      {/*<form>*/}
-      {/*<div className="form-group">*/}
-      {/*<label htmlFor="username">Login</label>*/}
-      {/*<input type="text" className="form-control form-control-sm" id="username" placeholder="Login"/>*/}
-      {/*</div>*/}
-      {/*<div className="form-group">*/}
-      {/*<label htmlFor="email">Email</label>*/}
-      {/*<input type="email" className="form-control form-control-sm" id="email" placeholder="name@example.com"/>*/}
-      {/*<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>*/}
-      {/*</div>*/}
-      {/*<div className="form-group">*/}
-      {/*<label htmlFor="password">Password</label>*/}
-      {/*<input type="password" className="form-control form-control-sm" id="password" placeholder="Password"/>*/}
-      {/*</div>*/}
-      {/*<div className="form-group">*/}
-      {/*<label htmlFor="password">Confirm password</label>*/}
-      {/*<input type="password" className="form-control form-control-sm" id="confirmPassword" placeholder="Confirm password"/>*/}
-      {/*</div>*/}
-      {/*</form>*/}
-      {/*</div>*/}
-      {/*<div className="modal-footer">*/}
-      {/*<button type="button" className="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>*/}
-      {/*<button type="button" className="btn btn-primary btn-sm">Sign Up</button>*/}
-      {/*</div>*/}
-      {/*</div>*/}
-      {/*</div>*/}
-      {/*</div>*/}
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Sign up</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label for="username">Login</Label>
+                <Input type="text" size="sm" id="username" placeholder="Login"/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input type="email" size="sm" id="email" placeholder="name@example.com" />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input type="password" size="sm" id="password" placeholder="Password"/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Confirm password</Label>
+                <Input type="password" size="sm" id="confirmPassword" placeholder="Confirm password"/>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" size="sm" onClick={this.toggleModal}>Close</Button>
+            <Button color="primary" size="sm">Sign Up</Button>
+          </ModalFooter>
+        </Modal>
     </Navbar>
     );
   }
