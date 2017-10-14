ec40d653c36252c3b9447cd350e5197a862f91b4
Vitaliy Senenko <senenkovitalik@gmail.com>
2017-10-14 17:10:36 +0300

    Remove unused files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import AppContainer from "./app_container";
-
-ReactDom.render(
-  <AppContainer />,
-  document.getElementById("app")
-);
