76e4ca564600551516c58b79c5496b072e94ded0
Vitaliy Senenko <senenkovitalik@gmail.com>
2017-11-05 21:14:46 +0200

    fix: contact page column width

--- client/src/components/contact_us.js
+++ client/src/components/contact_us.js
@@ -7,29 +7,17 @@ class ContactUs extends React.Component {
     return (
       <Container fluid>
         <Row>
-          <Col lg="6">
+          <Col lg="6" xs="12">
             <img src="/guitarshop.jpg" className="w-100 my-2" alt="GuitarShop" />
           </Col>
-          <Col lg="6">
+          <Col lg="6" xs="12">
             <h2 className="mt-2">GuitarShop</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             Email: guitarshop@gmail.com<br />
             Phone: +38(093)-234-45-56<br />
-            <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com/vitalik.senenko">
-              <span className="fa fa-facebook" style={{color: '#ffffff'}}></span>
-            </a>
-            <a className="btn btn-social-icon btn-google" href="https://plus.google.com/u/0/118132064331223916318">
-              <span className="fa fa-google-plus" style={{color: '#ffffff'}}></span>
-            </a>
-            <a className="btn btn-social-icon btn-instagram" href="https://www.instagram.com">
-              <span className="fa fa-instagram" style={{color: '#ffffff'}}></span>
-            </a>
           </Col>
         </Row>
         <Row>
-          {/*<Col lg="6">*/}
-            {/*<div id="map" className="my-2 w-100" style={{height: 400+'px'}}></div>*/}
-          {/*</Col>*/}
         </Row>
       </Container>
     );
